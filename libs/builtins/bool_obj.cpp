bff30680d9f22b6f5477a8a83796f92c5438f3af
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-12-13 10:14:12 +0800

    更新 bool_obj.cpp

--- libs/builtins/bool_obj.cpp
+++ libs/builtins/bool_obj.cpp
@@ -2,7 +2,7 @@
 
 namespace model {
 
-// 布尔值相等判断：self == args[0]（仅支持Bool与Bool比较）
+// Bool.__eq__ 布尔值相等判断：self == args[0]（仅支持Bool与Bool比较）
 model::Object* bool_eq(model::Object* self, const model::List* args) {
     DEBUG_OUTPUT("You given " + std::to_string(args->val.size()) + " arguments (bool_eq)");
     assert(args->val.size() == 1 && "function Bool.eq need 1 arg");
